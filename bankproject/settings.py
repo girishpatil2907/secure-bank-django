9b65f14b8efdaf475fb4e7624520af65196ac05d
girishpatil2907 <girishpatil2908@gmail.com>
2025-09-25 16:24:24 +0530

    updated database setting

--- bankproject/settings.py
+++ bankproject/settings.py
@@ -65,12 +65,23 @@ WSGI_APPLICATION = 'bankproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
+import dj_database_url
+import os
+
+# Check if running on Render (Render provides DATABASE_URL in env)
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    # Local dev: Use SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # Password validation
 # ... (AUTH_PASSWORD_VALIDATORS remains the same) ...
